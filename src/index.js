31ea6cdf46964f0980592a861602e2a3cc734bc8
Denis Klimenko <jaschik26@yandex.ru>
2021-03-10 22:16:01 +0400

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let result = str;
+
+  for (let i = 0; i <= str.length/2; i++) {
+    bracketsConfig.forEach(element => {
+      let closedBrackets = String(element[0] + element[1]);
+      if (result.includes(closedBrackets)) {
+        result = result.replace(closedBrackets,'');
+      };
+    });
+  };
+
+  if (!result) {
+    return true
+  } else {
+    return false
+  };
 }
